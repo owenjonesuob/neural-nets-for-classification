b04de71087e971243b994f66b8e8ebe5bb81fd51
owenjonesuob <olj23@bath.ac.uk>
2019-04-23 21:08:41 +0100

    Add more plots

--- net/kmnist.py
+++ net/kmnist.py
@@ -7,9 +7,10 @@ import utils
 
 kmnist_imgs = np.load("../data/kmnist-train-imgs.npz")["arr_0"]
 print("kmnist shape:", kmnist_imgs.shape)
-kmnist_imgs = kmnist_imgs.reshape(-1, 784)#[0:1000, ]
+kmnist_imgs = utils.scale_minmax(kmnist_imgs.reshape(-1, 784))[0:10000, ]
+print("kmnist reshaped:", kmnist_imgs.shape)
 
-kmnist_labs = np.load("../data/kmnist-train-labels.npz")["arr_0"]#[0:1000, ]
+kmnist_labs = np.load("../data/kmnist-train-labels.npz")["arr_0"][0:10000, ]
 print("labs shape:", kmnist_labs.shape)
 
 X_train, y_train, X_val, y_val, X_test, y_test = make_sets(kmnist_imgs, kmnist_labs, [0.6, 0.2, 0.2])
@@ -18,18 +19,44 @@ X_train, y_train, X_val, y_val, X_test, y_test = make_sets(kmnist_imgs, kmnist_l
 model = Network(layers = [
     Input(784),
     Dense(784, 100, "sigmoid"),
-    Dense(100, 40, "sigmoid"),
-    Dense(40, 10, "softmax")
+    Dense(100, 50, "sigmoid"),
+    Dense(50, 10, "softmax")
 ])
 
 model.reset()
-success = model.train(X_train, y_train, X_val, y_val, epochs=100, batch_size=512, learning_rate=10, penalty=0.1, early_stopping=10)
+success = model.train(X_train, y_train, X_val, y_val, epochs=500, batch_size=128, learning_rate=10, penalty=0.1, early_stopping=10)
 utils.plot_cost_curves(model)
 
 preds = model.predict(X_val)
 print("Confusion matrix:\n", utils.confusion_matrix(preds, y_val))
 #utils.plot_boundaries(model, X, y, subdivs=150)
 
+import matplotlib.pyplot as plt
+plt.matshow(X_train[0, :].reshape(28, 28), cmap="Greys_r")
+plt.show()
 
+plt.subplots(5, 5)
+
+for k in range(25):
+    plt.subplot(5, 5, k+1)
+    plt.imshow(model.layers[1].weights[k, 1:].reshape(28, 28), cmap="Greys_r", origin="upper", interpolation="nearest")
+
+plt.show()
+
+
+# Examples of: correct in first 5, incorrect in 6th col
+plt.subplots(10, 6)
+for row in range(10):
+    correct_idxs = [i for i, x in enumerate(preds == y_val) if preds[i] == row and x]
+    incorrect_idxs = [i for i, x in enumerate(preds == y_val) if preds[i] == row and not x]
+    for col in range(5):
+        plt.subplot(10, 6, 6*row + col + 1)
+        plt.imshow(X_val[correct_idxs[col], :].reshape(28, 28), cmap="Greys_r", origin="upper", interpolation="nearest")
+        plt.axis("off")
+    plt.subplot(10, 6, 6*row + 5 + 1)
+    plt.imshow(X_val[incorrect_idxs[0], :].reshape(28, 28), cmap="Greys_r", origin="upper", interpolation="nearest")
+    plt.axis("off")
+
+plt.show()
 
 
